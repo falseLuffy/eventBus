7db890be89cc349301da13d5ac263fa9a1260c34
falseLuffy <falseLuffy@qq.com>
2022-03-17 11:57:18 +0800

    feat(eventbus)增加on，once，emit，off方法

--- src/index.js
+++ src/index.js
@@ -1,21 +1,77 @@
+import { toArray } from './utils'
+
 export default class EventBus {
   constructor () {
     this.events = Object.create(null)
   }
 
   on (event, fn) {
-
+    const that = this
+    if (Array.isArray(event)) {
+      event.forEach(name => {
+        that.on(name, fn)
+      })
+    } else {
+      (that.events[event] && (that.events[event] = [])).push(fn)
+    }
+    return that
   }
 
   emit (event) {
-
+    const that = this
+    const cbs = that.events[event]
+    if (cbs) {
+      const args = toArray(arguments, 1)
+      cbs.forEach(cb => {
+        cb.apply(that, args)
+      })
+    }
+    return that
   }
 
   once (event, fn) {
+    const that = this
+    function on () {
+      that.off(event, on)
+      fn.apply(that, arguments)
+    }
+    on.fn = fn
+    that.on(event, on)
 
+    return that
   }
 
   off (event, fn) {
+    const that = this
+    if (!arguments.length) {
+      that.events = Object.create(null)
+      return that
+    }
 
+    if (Array.isArray(event)) {
+      event.forEach(name => {
+        that.off(name)
+      })
+      return that
+    }
+
+    const cbs = that.events[event]
+
+    if (!cbs) return that
+    if (!fn) {
+      that.events[event] = null
+      return that
+    }
+    // specific handler
+    let cb
+    let i = cbs.length
+    while (i--) {
+      cb = cbs[i]
+      if (cb === fn || cb.fn === fn) {
+        cbs.splice(i, 1)
+        break
+      }
+    }
+    return that
   }
 }
